d9de0b16fe90893f067c7712ead92cd409520489
manuelmolinaf <manuelmolinaf@outlook.com>
2023-01-04 21:06:39 -0400

    renamed project

--- src/app/components/main-view/main-view.component.ts
+++ src/app/components/main-view/main-view.component.ts
@@ -51,10 +51,27 @@ export class MainViewComponent {
     request.subscribe( data => this.status = data);
   }
 
-  getChartData(){
-    this.glucoseValues.map(value => {
+  setChartData(){
+    var data = {
+      datasets: [
+        {
+          data: [ 65, 59, 80, 81, 56, 55, 40 ],
+          label: 'Series A',
+          backgroundColor: 'rgba(148,159,177,0.2)',
+          borderColor: 'rgba(148,159,177,1)',
+          pointBackgroundColor: 'rgba(148,159,177,1)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+          fill: 'origin',
+        },
+      ],
+      labels: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July' ]
+    };
 
-    })
+    //do stuff
+
+    this.lineChartData = data;
   }
 
 

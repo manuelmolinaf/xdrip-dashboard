c66c870b455f74270d5415c85877e949984944b7
manuelmolinaf <manuelmolinaf@outlook.com>
2023-03-21 15:29:25 -0400

    added mock data

--- src/services/xdrip.service.ts
+++ src/services/xdrip.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { GlucoseValue } from '../models/api/GlucoseValue';
 import { Status } from 'src/models/api/Status';
 
@@ -12,14 +11,18 @@ export class XdripService {
 
   xdripUrl = 'http://10.0.0.52:17580'
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    
+   }
 
   public getGlucoseValues(count:number):Observable<GlucoseValue[]>{
-    return this.http.get<GlucoseValue[]>(this.xdripUrl + '/sgv.json?count=' + count.toString());
+    //return this.http.get<GlucoseValue[]>(this.xdripUrl + '/sgv.json?count=' + count.toString());
+    return this.http.get<GlucoseValue[]>("../assets/GLUCOSE_VALUES.json");
   }
 
   public getStatus():Observable<Status>{
-    return this.http.get<Status>(this.xdripUrl + '/status.json');
+    //return this.http.get<Status>(this.xdripUrl + '/status.json');
+    return this.http.get<Status>("../assets/STATUS.json");
   }
 
 
